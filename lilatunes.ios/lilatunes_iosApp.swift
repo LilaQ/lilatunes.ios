4cc9141baf895df3f559761d461c4c50eae4b265
LilaQ <teh.LilaQ@googlemail.com>
2025-06-18 20:32:24 +0200

    Initial Commit

---
+++ lilatunes.ios/lilatunes_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lilatunes_iosApp.swift
+//  lilatunes.ios
+//
+//  Created by Jan Sallads on 18.06.25.
+//
+
+import SwiftUI
+
+@main
+struct lilatunes_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lilatunes.iosTests/lilatunes_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  lilatunes_iosTests.swift
+//  lilatunes.iosTests
+//
+//  Created by Jan Sallads on 18.06.25.
+//
+
+import Testing
+@testable import lilatunes_ios
+
+struct lilatunes_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
